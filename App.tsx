2dd37067253350408b3c1e03cf61f9ea347a9cf5
simon.poppinga <simon.poppinga@bluehands.de>
2024-01-22 15:54:28 +0800

    Typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const login = () => {};
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hallo bitte logge dich ein oder registriere dich</Text>
+      <Button title='Login' onPress={login}/>
       <StatusBar style="auto" />
     </View>
   );
